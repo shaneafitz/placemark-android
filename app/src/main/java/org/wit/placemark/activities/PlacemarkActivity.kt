3042fdf886fa781a5ab773e5703237a2818d48c2
Shane Fitzpatrick <fitzpatrick.shane@gmail.com>
2021-09-21 17:07:37 +0100

    Finish of lab A02 Version 0.0

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,46 @@
+package org.wit.placemark.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.material.snackbar.Snackbar
+import org.wit.placemark.databinding.ActivityPlacemarkBinding
+import org.wit.placemark.models.PlacemarkModel
+import timber.log.Timber
+import timber.log.Timber.i
+
+
+class PlacemarkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlacemarkBinding
+    var placemark = PlacemarkModel()
+    val placemarks = ArrayList<PlacemarkModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Placemark Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            placemark.title = binding.placemarkTitle.text.toString()
+            placemark.description = binding.description.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                placemarks.add(placemark.copy())
+                i("add Button Pressed: ${placemark}")
+                for (i in placemarks.indices) {
+                    i("Placemark[$i]:${this.placemarks[i]}")
+                }
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+}
